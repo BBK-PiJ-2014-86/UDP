c528d14372ed1c2fc28545b2818c4bfe592f942e
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2015-04-22 18:54:57 +0100

    add while loop to listen all the time

--- AudioServerLaunch.java
+++ AudioServerLaunch.java
@@ -1,62 +1,31 @@
 package UDP;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+
 
 
 public class AudioServerLaunch {
 	
 	private static final int PORT = 12321; // server will listen on randomly selected port which is > 1024
-	private static boolean first = true; // this variable will act as a 'switch' to identify if first.
+	private static ServerSocket server;
 	
 	public static void main (String [] args) {
+		
+		while (true) {
+			
+			try {
+				server = new ServerSocket (PORT);
+				Socket socket = server.accept();
+				
 
-		 try  {
+			} catch (IOException e) {
+				System.out.println("Server could not connect. Port is in use.");
+				e.printStackTrace();
+			}
 			 
-		       ServerSocket server = new ServerSocket (PORT);
-			   Socket socket = server.accept();
-			   PrintWriter out = new PrintWriter (socket.getOutputStream());
-			   BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-			   assignUniqueId();
-			 
- 
-		 } catch (IOException e) {
-			e.printStackTrace();
 		}
-		
-		
 	 }
-
-	private void StreamToRestOfClients() {
-	
-		
-	}
-
-	private void StreamFromFirstClient() {
-
-		
-	}
-
-	private static int assignUniqueId() {
-		
-		
-		return 0;
-		
-		
-	}
-
-	private static boolean checkIfFirst() {
-		
-		
-		return first;
-		
-		
-	}
-
-	}
+}
 	 
